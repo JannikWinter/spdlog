1ae7c0138535523e06d3e51688e7577cec95c269
Jannik <jannik.winter@schaefer-it-solutions.com>
2022-06-22 10:49:30 +0200

    add premake script

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+spdlogPath = "%{wks.location}/dependencies/spdlog"
+spdlogBuildPath = "%{spdlogPath}/build/%{cfg.platform}_%{cfg.buildcfg}"
+
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	targetdir "%{spdlogBuildPath}"
+
+	defines {
+		"SPDLOG_COMPILED_LIB"
+	}
+
+	files {
+		"include/spdlog/**.h",
+		"src/spdlog.cpp",
+		"src/stdout_sinks.cpp",
+		"src/color_sinks.cpp",
+		"src/file_sinks.cpp",
+		"src/async.cpp",
+		"src/cfg.cpp",
+		"src/fmt.cpp"
+	}
+
+	includedirs {
+		"include"
+	}
+
+	IncludeDir["spdlog"] = "%{spdlogPath}/include"
+	LibraryDir["spdlog"] = "%{spdlogBuildPath}"
+	Link["spdlog"] = "spdlog"
+
+	filter "configurations:Debug"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        runtime "Release"
